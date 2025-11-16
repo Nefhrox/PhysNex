8e91a0b40b05bb67caff8bd9d006d8a38cd6add3
Nefhrox <nefhrox@gmail.com>
2025-11-16 20:53:07 +0200

    modified script to make new files automatically with base code in html only text needed: text of a problem, answer, solution other already done by script in js file for now nothing should be changed buttons works automatically after using script all needed instructions in script comments

--- problem_creator.sh
+++ problem_creator.sh
@@ -1,8 +1,6 @@
-sudo dpkg -i package.deb or sudo apt install ./package.deb
-
 # EXECUTION: go to desireble directory(topic/sub-topic) launch script by "bash problem_creator.sh"
 # Write number of a problem you want to create
-# New directory with new HTML file, src directory and css file created
+# New directory with new html file, src directory, css file and js file created
 
 echo -n "Problem number: "
 read NUM
@@ -24,10 +22,121 @@ mkdir "$DIR"
 mkdir "./$DIR/src"
 
 
-# Debug 
-touch "$DIR/$FILE"
-touch "$DIR/style.css"
+#text that you want to create in script js when creating it
 
+js_script_text=$(cat << 'EOF'
+const ANSWER_BUTTON = document.getElementById("show_answer");
+const SOLUTION_BUTTON = document.getElementById("show_solution");
+
+const ANSWER = document.getElementById("answer");
+const SOLUTION = document.getElementById("solution");
+
+ANSWER_BUTTON.addEventListener("click", () => 
+{
+    if (ANSWER.style.display === "none" || ANSWER.style.display === "") {
+        ANSWER.style.display = "block";
+        ANSWER_BUTTON.textContent = "Hide answer";
+    } 
+    else {
+        ANSWER.style.display = "none";
+        ANSWER_BUTTON.textContent = "Show answer";
+    }
+});
+
+//same as ANSWER_BUTTON
+
+SOLUTION_BUTTON.addEventListener("click", () => 
+{
+    if (SOLUTION.style.display === "none" || SOLUTION.style.display === "") {
+        SOLUTION.style.display = "block";
+        SOLUTION_BUTTON.textContent = "Hide solution";
+    } 
+    else {
+        SOLUTION.style.display = "none";
+        SOLUTION_BUTTON.textContent = "Show solution";
+    }
+});
+EOF
+)
+
+
+
+#html text
+
+#IMPORTANT: problem number automatically imports to html file from user when he enter problem number 
+
+html_text=$(cat << EOF
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>Problem $NUM</h1>
+
+<h3>Text of a problem: </h3>
+
+
+
+<button id ="show_answer">Show Answer</button>
+<h3 id="answer">Answer: </h3>
+
+
+<button id="show_solution">Show solution</button>
+<h3 id="solution">Solution: </h3>
+
+<script src="script.js"></script>
+
+
+</body>
+
+</html> 
+EOF
+)
+
+
+
+css_text=$(cat << 'EOF'
+#show_solution {
+    font-size: medium;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+#show_answer {
+    font-size: medium;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+#answer {
+    display: none;
+}
+
+#solution {
+    display: none;
+}
+EOF
+)
+
+
+
+touch "$DIR/$FILE"       #html file
+touch "$DIR/style.css"   #css file
+touch "$DIR/script.js"   #js script
+
+echo "$js_script_text" >> "./$DIR/script.js"     #enter code in script js form variable "js_script_text"
+echo "$html_text" >> "./$DIR/$FILE"              #enter code in html file from variable "html_text"
+echo "$css_text" >> "./$DIR/style.css"           #IMPORTANT: save "$" for normal script work
+
+
+
+#Debug
 echo "Created directory: $DIR"
 echo "Created directory: $DIR/scr"
 
