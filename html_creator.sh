91489201600d4e897d670af447a59bf0a15fa091
nefhrox <nefhrox@gmail.com>
2025-11-28 18:02:27 +0200

    add "html_creator.sh" a script that allows to automatically create html and css files automatically for disered directory, execurion instructions in script file rename "Newton's_laws" into "Newton_s_laws" in case if occurs any problems with ' in directory name

--- html_creator.sh
+++ html_creator.sh
@@ -1,15 +1,38 @@
-CURRENT_DIR="$(pwd)"
-FILE="$(basename "$CURRENT_DIR")"
+#EXECUTION 
+#go to desireble directory(topic) and launch script by "bash ../../html_creator.sh"
+# "../../" only if you are in topics directory such as Mechanics, Electricity, etc.
 
 
-style_css_text=$(cat << 'EOF'
-.sub_topic {
+CURRENT_DIR="$(pwd)"                         #Current working directory
+FILE="$(basename "$CURRENT_DIR")"            #Name for files in current directory
+
+
+
+style_css_text=$(cat << EOF                 #text for css file for that page
+.sub_topic {                                
     margin-top: 4px;
 }
 EOF
 )
 
-html_text=$(cat << 'EOF'
+
+
+
+#loop that creates html file with own name in each subdirectory only if its not src or problem and is a directory
+
+for DIR in *; do
+
+    if [ -d "$DIR" ] && [[ "$DIR" != "src" && "$DIR" != problem_* ]]; then  # Before -d must be space or script wont work 
+
+        pushd "$DIR" >> /dev/null    #creal output of pushd command
+
+
+        mkdir -p "src"               #create src directory if it doesnt exist
+        touch "./src/style.css"
+        touch "$DIR.html"
+
+
+html_text=$(cat << EOF
 <!DOCTYPE html>
 
 <html>
@@ -17,14 +40,14 @@ html_text=$(cat << 'EOF'
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./src/style.css">
-    <title></title>
+    <title>Problems $DIR</title>
 </head>
 
 <body>
 
-<h1></h1>
+<h1>Problems on $DIR</h1>
 
-<h3></h3>
+<h3>List of problems on $DIR</h3>
 
 <ol>
     <li class="problem"><a href="./problem_1/problem_1.html">Problem 1</a></li>
@@ -37,14 +60,12 @@ EOF
 )
 
 
+        echo "$style_css_text" > "./src/style.css"
+        echo "$html_text" > "$DIR.html"
 
-echo "$style_css_text" > "$CURRENT_DIR/src/style.css"
-echo "$html_text" > "$CURRENT_DIR/$FILE.html"
+
+        popd >> /dev/null           #clear output of popd command
+    fi
+done
 
 
-echo " "
-echo "$CURRENT_DIR"
-echo "current dir"
-echo " "
-echo "file name:"
-echo "$FILE"
