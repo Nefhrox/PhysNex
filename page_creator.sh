af637ceec5bc2909b0e7dcd48e15a75ea158c1c9
nefhrox <nefhrox@gmail.com>
2025-11-28 22:08:41 +0200

    add logo to main page add footer with link to discord server create pages for every sub-topic using page_creator

--- page_creator.sh
+++ page_creator.sh
@@ -5,12 +5,47 @@
 
 CURRENT_DIR="$(pwd)"                         #Current working directory
 FILE="$(basename "$CURRENT_DIR")"            #Name for files in current directory
-
+TOPIC_DIR="$(dirname "$CURRENT_DIR")"        #Topic directory path
 
 
 style_css_text=$(cat << EOF                 #text for css file for that page
 .sub_topic {                                
     margin-top: 4px;
+}
+.footer {
+    margin-top: 600px;
+    text-align: center;
+    font-size: 50px;
+}
+
+.link {
+    text-align: center;
+    font-size: 40px;
+    margin-top: 10px;
+}
+.discord_icon {
+    width: 70px;
+    height: 45px;
+    margin-right: 7px;
+    vertical-align: middle;
+}
+
+#sub_topic {
+    margin-bottom: 30px;
+    text-align: center;
+    font-size: 40px;
+}
+
+#list {
+    margin-top: 20px;
+    font-size: 35px;
+    margin-left: 7px;
+}
+
+.problem {
+    font-size: 20px;
+    margin-left: 7px;
+    margin-bottom: 50px;
 }
 EOF
 )
@@ -57,13 +92,16 @@ html_text=$(cat << EOF
 
 <body>
 
-<h1>Problems on $DISPLAY_TITLE</h1>
+<h1 id="sub_topic">Problems on $DISPLAY_TITLE</h1>
 
-<h3>List of problems on $DISPLAY_TITLE</h3>
+<h3 id="list">List of problems on $DISPLAY_TITLE</h3>
 
 <ol>
     <li class="problem"><a href="./problem_1/problem_1.html">Problem 1</a></li>
 </ol>
+
+<h2 class="footer">About:</h2>
+<p class="link"><a href="https://discord.gg/xFGw5Spd5V"><img src="../../../styles/discord_icon.png" class = "discord_icon" alt="Discord">Discord</a></p>
 
 </body>
 
