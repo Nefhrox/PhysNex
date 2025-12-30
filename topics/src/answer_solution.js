c1fc861be76226517f9910a2d41cbf34505ebc58
nefhrox <nefhrox@gmail.com>
2025-12-30 20:12:22 +0200

    add additional variables for answer_solution script in case in answer will be images and add chacges corresponding changes to css file for problems for proper work add automatic creation of place for images in answer add problems to optics sub-topics

--- topics/src/answer_solution.js
+++ topics/src/answer_solution.js
@@ -1,42 +1,41 @@
+//for buttons
 const ANSWER_BUTTON = document.getElementById("show_answer");
 const SOLUTION_BUTTON = document.getElementById("show_solution");
 
+//for solution and answer text/images
 const ANSWER = document.getElementById("answer");
-const SOLUTION = document.getElementById("solution");
-const SOLUTION_HEADING = document.getElementById("solution-heading");
+const SOLUTION = document.getElementById("solution");    
+const ANSWER_IMAGE = document.getElementById("answer_image");          //for image in answer            
+const SOLUTION_IMAGE = document.getElementById("solution_image");      //for image in solution
 
 
 
-ANSWER_BUTTON.addEventListener("click", () => 
-    {
-    if (ANSWER.style.display === "none" || ANSWER.style.display === "")  // check if text is hidden
-        {
-        ANSWER.style.display = "block";
-        ANSWER_BUTTON.textContent = "Hide answer";       //if text is hidden change button text and show answer
-    } 
-    else 
-        {
-        ANSWER.style.display = "none";
-        ANSWER_BUTTON.textContent = "Show answer";        //if text isnt hidden change button text and hide solution
+ANSWER_BUTTON.addEventListener("click", () => {
+
+    let A_Element = ANSWER || ANSWER_IMAGE;      //check if elements exists
+    let A_is_Hidden = A_Element.style.display === "none" || A_Element.style.display === "";  //check if elements are hidden
+    if (A_is_Hidden) {
+        if(ANSWER) ANSWER.style.display = "block";
+        if(ANSWER_IMAGE) ANSWER_IMAGE.style.display = "block";
+        ANSWER_BUTTON.textContent = "Hide answer";
+    } else {
+        if(ANSWER) ANSWER.style.display = "none";
+        if(ANSWER_IMAGE) ANSWER_IMAGE.style.display = "none";
+        ANSWER_BUTTON.textContent = "Show answer";
     }
 });
 
+SOLUTION_BUTTON.addEventListener("click", () => {
+    let S_Element = SOLUTION || SOLUTION_IMAGE;      //check if elements exists
+    let S_is_Hidden = S_Element.style.display === "none" || S_Element.style.display === "";  //check if elements are hidden
 
-
-//same as ANSWER_BUTTON
-
-SOLUTION_BUTTON.addEventListener("click", () => 
-    {
-    if (SOLUTION.style.display === "none" || SOLUTION.style.display === "") 
-        {
-        SOLUTION_HEADING.style.display = "block";
-        SOLUTION.style.display = "block";
+    if (S_is_Hidden) {
+        if (SOLUTION) SOLUTION.style.display = "block";
+        if (SOLUTION_IMAGE) SOLUTION_IMAGE.style.display = "block";
         SOLUTION_BUTTON.textContent = "Hide solution";
-    } 
-    else 
-        {
-        SOLUTION_HEADING.style.display = "none";
-        SOLUTION.style.display = "none";
+    } else {
+        if (SOLUTION) SOLUTION.style.display = "none";
+        if (SOLUTION_IMAGE) SOLUTION_IMAGE.style.display = "none";
         SOLUTION_BUTTON.textContent = "Show solution";
     }
 });
