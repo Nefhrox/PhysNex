e24392cfe7748bfa5cd7b4252f025efc9fce6075
nefhrox <nefhrox@gmail.com>
2026-03-18 22:08:08 +0200

    add sub-topics to topic.html from db

--- src/load_topic.js
+++ src/load_topic.js
@@ -10,24 +10,47 @@ async function load_topic() {
         return;
     }
 
-    const { data: topic_data, error } = await Supabase
-        .from('topics')
-        .select('*')
-        .eq('id', topic_id) 
-        .single();
+    const {data: topic, error: topics_error} = await Supabase
+    .from('topics')
+    .select('id, name, sub_topics (id, name)')
+    .eq('id', topic_id)
+    .single();
 
-    if (error || !topic_data) {
-        console.error("Error loading topic:", error);
+    if (topics_error || !topic) {
+        console.error("Error loading topic:", topics_error);
         document.getElementById('topic').innerText = "Topic not found";
         return;
     }
 
-    console.log("Topic data:", topic_data);
+    console.log("Topic data:", topic);
 
-    const topic_text = topic_data.name.replace(/_/g, ' ');
+    const topic_text = topic.name.replace(/_/g, ' ');
 
-    document.getElementById('topic').innerText = topic_text;
+    document.getElementById("topic").innerHTML = topic_text;
+    document.getElementById("about_topic").innerHTML = topic_text;
+    document.title = topic_text;
+
+    let html = "";
+
+    const sub_topics = topic.sub_topics;
+
+    topic.sub_topics.forEach((sub_t, i) => {
+
+        const is_last_el = (i === sub_topics.length - 1);
+        const is_odd = (i % 2 === 1);
+        let style = "";
+
+        if(is_odd && is_last_el)
+        {
+            style = 'style="margin: 0px 0px 0px 58%; width: 80%;"';
+        }
+
+        html += `<li class="card" ${style}><a href="../template/sub_topic.html?id=${sub_t.id}" class="sub_toppic">${sub_t.name.replace(/_/g, " ")}</a></li>`;
+
+    });
+
+    document.getElementById("sub_topics_grid").innerHTML = html;
 
 }
 
-window.onload = load_topic;
+load_topic();
