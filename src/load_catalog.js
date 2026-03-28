246f6adcf467bd44d11683d4d742700e020ad747
nefhrox <nefhrox@gmail.com>
2026-03-28 20:14:23 +0200

    switch from local images to images for db

--- src/load_catalog.js
+++ src/load_catalog.js
@@ -7,21 +7,37 @@ async function load_catalog()
 
     const {data: topic, error: topics_error} = await Supabase
     .from('topics')
-    .select('id, name, sub_topics (id, name)');
+    .select('id, name, image_name, sub_topics (id, name)');
 
     if (topics_error) 
     {
         console.error("Error loading topics ", topics_error);
-        document.getElementById('topics').innerText = "Error loading topics";
+        document.getElementById('data_container').innerText = "Error loading topics";
     }
 
     let html = "";
 
+    const bucket = Supabase.storage.from('style images');
+
+    const logo = bucket.getPublicUrl('physnex.png');
+    const discord_icon = bucket.getPublicUrl('discord_icon.png');
+    const gmail_icon = bucket.getPublicUrl('gmail_icon.png');
+
+    document.getElementById('physnex').src = logo.data.publicUrl;
+    document.getElementById('discord_icon').src = discord_icon.data.publicUrl;
+    document.getElementById('gmail_icon').src = gmail_icon.data.publicUrl;
 
 
     topic.forEach(topic => {
 
-        html += `<li class="topic"><div class="topic_header"><a href="./template/topic.html?id=${topic.id}">${topic.name.replace(/_/g, " ")}</a></div><ul>`;
+        const img_name = topic.image_name;
+        const img_url = bucket.getPublicUrl(img_name).data.publicUrl;
+
+        html += `<li class="topic">
+        <div class="topic_header">
+        <a href="./template/topic.html?id=${topic.id}">${topic.name.replace(/_/g, " ")}</a>
+        <img src="${img_url}.png" class="topic_image"></img></div><ul>`;
+        console.log("img url ", img_url);
 
         topic.sub_topics.forEach(sub_t => {
             html += `<li class="sub_topic"><a href="./template/sub_topic.html?id=${sub_t.id}">${sub_t.name.replace(/_/g, " ")}</a></li>`;
