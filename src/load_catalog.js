44d6a1ac4537109c40d18ae33a7cd3f1b9402c8e
nefhrox <nefhrox@gmail.com>
2026-04-04 10:04:00 +0300

    fix link to images on every page

--- src/load_catalog.js
+++ src/load_catalog.js
@@ -20,12 +20,8 @@ async function load_catalog()
     const bucket = Supabase.storage.from('style images');
 
     const logo = bucket.getPublicUrl('physnex.png');
-    const discord_icon = bucket.getPublicUrl('discord_icon.png');
-    const gmail_icon = bucket.getPublicUrl('gmail_icon.png');
 
     document.getElementById('physnex').src = logo.data.publicUrl;
-    document.getElementById('discord_icon').src = discord_icon.data.publicUrl;
-    document.getElementById('gmail_icon').src = gmail_icon.data.publicUrl;
 
 
     topic.forEach(topic => {
